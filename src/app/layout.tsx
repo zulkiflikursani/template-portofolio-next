56b4054135a3e1cc2984ad9ff6d1d9e8ca7516ab
zulkiflikursani <69183966+zulkiflikursani@users.noreply.github.com>
2024-03-07 01:43:53 +0800

    scale meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,6 @@ export const metadata: Metadata = {
 };
 export const viewport: Viewport = {
   width: "device-width",
-  initialScale: 1,
-  maximumScale: 1,
   userScalable: false,
   // Also supported by less commonly used
   // interactiveWidget: 'resizes-visual',
